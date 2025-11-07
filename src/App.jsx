eb8be22b2d6bc17c8aa524cd49395b7c2b376fe0
paridhiramtani <ramtaniparidhi@gmail.com>
2025-11-07 18:59:50 +0530

    Create App component with TDSExamHelper

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TDSExamHelper from "./components/TDSExamHelper";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <TDSExamHelper />
+    </div>
+  );
+}
